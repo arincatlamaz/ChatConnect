88c7086a89b78aa8aacf5860f3c1e9980c2f2e64
arincatlamaz <arincatlamaz@gmail.com>
2023-07-06 20:33:49 +0300

    .

--- app/src/main/java/com/arincatlamaz/chatconnect/view/MainActivity.kt
+++ app/src/main/java/com/arincatlamaz/chatconnect/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.arincatlamaz.chatconnect
+package com.arincatlamaz.chatconnect.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arincatlamaz.chatconnect.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
