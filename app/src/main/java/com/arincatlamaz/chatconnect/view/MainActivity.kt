16cbf88e062a625018468ae914f4c3151ba22b06
arincatlamaz <arincatlamaz@gmail.com>
2023-07-10 19:12:59 +0300

    .

--- app/src/main/java/com/arincatlamaz/chatconnect/view/MainActivity.kt
+++ app/src/main/java/com/arincatlamaz/chatconnect/view/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.arincatlamaz.chatconnect.view
 
-import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.statusBarColor = Color.TRANSPARENT
-        window.navigationBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+
     }
 }
