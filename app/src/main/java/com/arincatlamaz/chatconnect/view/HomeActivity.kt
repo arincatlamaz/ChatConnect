beed1186c0135edf976c6f2d50a0dca3eed761a2
arincatlamaz <arincatlamaz@gmail.com>
2023-07-13 01:16:22 +0300

    Initial commit

--- app/src/main/java/com/arincatlamaz/chatconnect/view/HomeActivity.kt
+++ app/src/main/java/com/arincatlamaz/chatconnect/view/HomeActivity.kt
@@ -1,16 +1,24 @@
 package com.arincatlamaz.chatconnect.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.arincatlamaz.chatconnect.R
 import com.arincatlamaz.chatconnect.databinding.ActivityHomeBinding
+import com.arincatlamaz.chatconnect.model.User
+import com.arincatlamaz.chatconnect.viewmodel.SearchViewModel
 
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-
+    private lateinit var viewModel: SearchViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +49,49 @@ class HomeActivity : AppCompatActivity() {
             }
         }
 
+        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.search_list_item, R.id.list_username)
+
+        binding.listview.adapter = adapter
+
+        adapter.notifyDataSetChanged()
+
+        binding.constraintLayout2.setOnClickListener {
+            binding.searchView.clearFocus()
+            adapter.clear()
+        }
+
+        binding.searchView.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("beforeTextChanged","beforeTextChanged")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.searchUsers(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.d("afterTextChanged","afterTextChanged")
+            }
+
+        })
+
+        viewModel.searchResults.observe(this) { users ->
+            if (binding.searchView.text.isNotEmpty()){
+                val usernames = users.map { it.username }
+                adapter.clear()
+                adapter.addAll(usernames)
+            }else{
+                adapter.clear()
+            }
+
+
+        }
+
     }
+
+
+
 }
 
---
+++ app/src/main/java/com/arincatlamaz/chatconnect/viewmodel/SearchViewModel.kt
@@ -0,0 +1,58 @@
+package com.arincatlamaz.chatconnect.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arincatlamaz.chatconnect.model.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resumeWithException
+
+class SearchViewModel : ViewModel() {
+
+    private val _searchResults = MutableLiveData<List<User>>()
+    val searchResults: LiveData<List<User>> get() = _searchResults
+
+    private val databaseRef = FirebaseDatabase.getInstance().reference.child("users")
+    private var currentSearchQuery: String = ""
+
+    fun searchUsers(searchText: String) {
+        currentSearchQuery = searchText
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        databaseRef.orderByChild("username")
+            .startAt(currentSearchQuery)
+            .endAt(currentSearchQuery + "\uf8ff")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val users = mutableListOf<User>()
+                    for (snapshot in dataSnapshot.children) {
+                        val username = snapshot.child("username").value.toString()
+                        val user = User(username)
+                        users.add(user)
+                    }
+                    _searchResults.value = users
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("DBERROR", error.message)
+                }
+            })
+    }
+
+    fun displaySearchResults(users: List<User>) {
+        val resultText = StringBuilder()
+        for (user in users) {
+            resultText.append(user.username).append("\n")
+        }
+        Log.d("result:",resultText.toString())
+    }
+}
